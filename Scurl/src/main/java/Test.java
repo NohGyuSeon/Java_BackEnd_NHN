2ef37f1ec4ccd5bdd0c9125a539ef4d7ba4c7144
NGS <kusun1020@gmail.com>
2023-03-27 01:42:07 +0900

    Delete Scurl directory

--- Scurl/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Test {
-
-    public static void main(String[] args) throws IOException {
-        Scurl scurl = new Scurl(args);
-        scurl.start();
-    }
-}
